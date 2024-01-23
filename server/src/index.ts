744a66dbe8e82405e9834e952457f2f1164ddbc2
steven-nguyen22 <snguy117@ucr.edu>
2024-01-22 23:16:35 -0800

    render backend update

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://stevens-budgetfy.netlify.app",
   })
 );
 app.use(express.json());
